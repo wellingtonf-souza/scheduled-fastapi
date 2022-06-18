536db57acc2199bbd69e5f44bd77a0f31ad59c0d
Wellington Ferreira <wellingtonf.souza@outlook.com>
2022-06-18 12:57:12 -0300

    refactor: coloca a rota de listar os jobs dentro da pasta routes

--- server.py
+++ server.py
@@ -8,21 +8,6 @@ import json
 import warnings
 warnings.filterwarnings("ignore")
 
-@app.get("/jobs")
-def list_jobs(response: Response):
-    """List jobs
-    """
-    jobs = []
-    for job in scheduler.get_jobs():
-        jobs.append({
-            "job_id": str(job.id), 
-            "description": str(job.name), 
-            "run_frequency": str(job.trigger), 
-            "next_run": str(job.next_run_time)
-        })
-    response.status_code = status.HTTP_200_OK
-    return jobs
-
 def load_schedule():
     scheduler.start()
     logger.info("Init Schedule")
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,3 +1,4 @@
 from .healthcheck import healthcheck
 from .lottery import lottery
 from .sleep import sleep
+from .list_jobs import list_jobs
