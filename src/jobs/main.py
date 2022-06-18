89232414696107c2a4afc407efb33dc0439d27dd
Wellington Souza <wellington.souza@gerencianet.com.br>
2022-06-18 04:26:50 +0000

    fix: define corretamente o nome de cada job

--- src/jobs/main.py
+++ src/jobs/main.py
@@ -5,7 +5,7 @@ from src.jobs.interface import Job
 
 class Healthcheck(Job):
 
-    def __init__(self, name = 'healthcheck scheduler', trigger='interval', seconds=10, misfire_grace_time = None, hours = None):
+    def __init__(self, name = 'Healthcheck scheduler', trigger='interval', seconds=10, misfire_grace_time = None, hours = None):
         self._name = name
         self._trigger = trigger
         self._seconds = seconds
@@ -17,7 +17,7 @@ class Healthcheck(Job):
 
 class Sleep(Job):
 
-    def __init__(self, name = 'healthcheck scheduler', trigger='interval', seconds=20, misfire_grace_time = None, hours = None):
+    def __init__(self, name = 'Sleep scheduler', trigger='interval', seconds=20, misfire_grace_time = None, hours = None):
         self._name = name
         self._trigger = trigger
         self._seconds = seconds
@@ -25,5 +25,6 @@ class Sleep(Job):
         self._misfire_grace_time = misfire_grace_time
     
     def action(self):
-        time.sleep(2)
-        logger.info("scheduler main running")
+        logger.info("scheduler sleep init")
+        time.sleep(20)
+        logger.info("scheduler sleep finish")
