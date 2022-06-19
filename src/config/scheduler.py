b37505681129f228aa1697283214309ebf07e111
Wellington Ferreira <wellingtonf.souza@outlook.com>
2022-06-19 20:10:46 -0300

    refactor: especifica o logger do agendador

--- src/config/scheduler.py
+++ src/config/scheduler.py
@@ -1,7 +1,8 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor
+from src.logs import logger
 
 executors = {
     'default': ThreadPoolExecutor(20)
 }
-scheduler = BackgroundScheduler(executors=executors, timezone = "America/Sao_Paulo")
+scheduler = BackgroundScheduler(executors=executors, logger = logger, timezone = "America/Sao_Paulo")
